9789f8cf11d85379e8c4d511a06f550ae9761f5e
Pranav964 <pranavkulkarni964@gmail.com>
2026-01-30 12:52:04 +0530

    Add apps/quantitymeasurement package structure

--- demo/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementapp.java
+++ demo/src/main/java/com/apps/quantitymeasurement/QuantityMeasurementapp.java
@@ -1,10 +1,10 @@
-package com.example;
+package com.apps.quantitymeasurement;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class QuantityMeasurementapp 
 {
     public static void main( String[] args )
     {
